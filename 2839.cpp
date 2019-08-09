4e9bf37da1e5748d21d694763c0d019c47092101
Kim Beomsoo <kbs9407@naver.com>
2019-08-09 23:12:16 +0900

    SECER (2839)
    https://www.acmicpc.net/problem/2839

---
+++ 2839.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    cin >> n;
+    
+    while(1)
+    {
+        if(n % 5 == 0)
+        {
+            count += n/5;
+            break;
+        }
+        else if(n < 3)
+        {
+            count = -1;
+            break;
+        }
+        else
+        {
+            n -= 3;
+            count++;
+        }
+    }
+    
+    cout << count << endl;
+    
+    return 0;
+}
